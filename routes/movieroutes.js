71cef280dfc766b42def6849516cc50a29028db3
johnbabu021 <johnbabu021@gmail.com>
2022-07-06 23:52:33 +0530

    refractor:router module descriptions are added

--- routes/movieroutes.js
+++ routes/movieroutes.js
@@ -8,10 +8,29 @@ const movieRoute=express.Router()
 
 
 movieRoute.route('/movies')
+//@desc get whole movies
+//@route /api/movies
+//@access PUBLIC
+//@method:GET
 .get(findMovies)
+//@desc add a movie
+//@route /api/movies
+//@access PUBLIC
+//@method:POST
 .post(addMovies)
+
+
+
 movieRoute.route('/movies/:movieId')
+//@desc update a movie
+//@route /api/movies/:movieId
+//@access PRIVATE
+//@method:PUT
 .put(authMiddleware,updateMovies)
+//@desc delete a movie
+//@route /api/movies/:movieId
+//@access PRIVATE
+//@METHOD DELETE
 .delete(authMiddleware,deleteMovies)
 
 
